260be2b37e20c54b50851212368968aa597afd53
petko.lyutskanov <simona2000petko@abv.bg>
2022-11-13 13:27:47 +0200

    Binary search tree implemented

--- src/main/java/trees/BinarySearchTree.java
+++ src/main/java/trees/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
     public BinaryNode root;
 
@@ -61,5 +64,77 @@ public class BinarySearchTree {
         postOrder(node.left);
         postOrder(node.right);
         System.out.print(node.value + " ");
+    }
+
+    public void levelOrder() {
+        Queue<BinaryNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            BinaryNode presentNode = queue.remove();
+            System.out.print(presentNode.value + " ");
+
+            if (presentNode.left != null) {
+                queue.add(presentNode.left);
+            }
+            if (presentNode.right != null) {
+                queue.add(presentNode.right);
+            }
+        }
+    }
+
+    public BinaryNode search(BinaryNode node, int value) {
+        if (node == null) {
+            System.out.println("Value " + value + " not found in BST");
+            return null;
+        } else if (node.value == value) {
+            System.out.println("Value " + value + " found in BST");
+            return node;
+        } else if (value < node.value) {
+            return search(node.left, value);
+        } else {
+            return search(node.right, value);
+        }
+    }
+
+    public static BinaryNode minimumNode(BinaryNode root) {
+        if (root.left == null) {
+            return root;
+        } else {
+            return minimumNode(root.left);
+        }
+    }
+
+    public BinaryNode deleteNode(BinaryNode root, int value) {
+        if (root == null) {
+            System.out.println("Value not found in BST");
+            return null;
+        }
+
+        if (value < root.value) {
+            root.left = deleteNode(root.left, value);
+        } else if (value > root.value) {
+            root.right = deleteNode(root.right, value);
+        } else {
+            if (root.left != null && root.right != null) {
+                BinaryNode temp = root;
+                BinaryNode minNodeForRight = minimumNode(temp.right);
+                root.value = minNodeForRight.value;
+                root.right = deleteNode(root.right, minNodeForRight.value);
+            } else if (root.left != null) {
+                root = root.left;
+            } else if (root.right != null) {
+                root = root.right;
+            } else {
+                root = null;
+            }
+        }
+
+        return root;
+    }
+
+    public void deleteBST() {
+        root = null;
+        System.out.println("BST has been deleted successfully");
     }
 }
