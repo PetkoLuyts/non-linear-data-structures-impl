654386ae598d15196e1ef2b13be4920097f048e9
petko.lyutskanov <simona2000petko@abv.bg>
2022-11-27 17:14:32 +0200

    BFS in Adjacency Matrix implemented

--- src/main/java/graph/GraphAdjacencyMatrix.java
+++ src/main/java/graph/GraphAdjacencyMatrix.java
@@ -1,6 +1,7 @@
 package graph;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class GraphAdjacencyMatrix {
     ArrayList<GraphNode> nodeList = new ArrayList<>();
@@ -9,6 +10,7 @@ public class GraphAdjacencyMatrix {
     public static class GraphNode {
         public String name;
         public int index;
+        public boolean isVisited = false;
 
         public GraphNode(String name, int index) {
             this.name = name;
@@ -46,5 +48,45 @@ public class GraphAdjacencyMatrix {
         }
 
         return s.toString();
+    }
+
+    public ArrayList<GraphNode> getNeighbours(GraphNode node) {
+        ArrayList<GraphNode> neighbours = new ArrayList<>();
+        int nodeIndex = node.index;
+
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            if (adjacencyMatrix[nodeIndex][i] == 1) {
+                neighbours.add(nodeList.get(i));
+            }
+        }
+
+        return neighbours;
+    }
+
+    public void bfsVisit(GraphNode node) {
+        LinkedList<GraphNode> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            GraphNode currentNode = queue.remove(0);
+            currentNode.isVisited = true;
+            System.out.print(currentNode.name + " ");
+            ArrayList<GraphNode> neighbours = getNeighbours(currentNode);
+
+            for (GraphNode neighbour : neighbours) {
+                if (!neighbour.isVisited) {
+                    queue.add(neighbour);
+                    neighbour.isVisited = true;
+                }
+            }
+        }
+    }
+
+    public void bfs() {
+        for (GraphNode node : nodeList) {
+            if (!node.isVisited) {
+                bfsVisit(node);
+            }
+        }
     }
 }
