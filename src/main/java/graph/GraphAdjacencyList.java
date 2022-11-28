dd74a65e2c76109ddc7b6165c3b108320fff07ca
petko.lyutskanov <simona2000petko@abv.bg>
2022-11-28 09:25:04 +0200

    DFS in Graph Adjacency List implemented

--- src/main/java/graph/GraphAdjacencyList.java
+++ src/main/java/graph/GraphAdjacencyList.java
@@ -2,6 +2,7 @@ package graph;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class GraphAdjacencyList {
     ArrayList<GraphNode> nodeList = new ArrayList<>();
@@ -70,6 +71,32 @@ public class GraphAdjacencyList {
         for (GraphNode node : nodeList) {
             if (!node.isVisited) {
                 bfsVisit(node);
+            }
+        }
+    }
+
+    public void dfsVisit(GraphNode node) {
+        Stack<GraphNode> stack = new Stack<>();
+        stack.push(node);
+
+        while (!stack.isEmpty()) {
+            GraphNode currentNode = stack.pop();
+            currentNode.isVisited = true;
+            System.out.print(currentNode.name + " ");
+
+            for (GraphNode neighbour : currentNode.neighbours) {
+                if (!neighbour.isVisited) {
+                    stack.push(neighbour);
+                    neighbour.isVisited = true;
+                }
+            }
+        }
+    }
+
+    public void dfs() {
+        for (GraphNode node : nodeList) {
+            if (!node.isVisited) {
+                dfsVisit(node);
             }
         }
     }
